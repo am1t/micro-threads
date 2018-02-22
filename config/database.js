03358ad999bef4ae055d42692b7dd43d2b469344
am1t <amit.w.gawande@gmail.com>
2018-02-23 01:09:15 +0530

    DB config change

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: "mongodb://mongo_am_user:D9w+[VybC6@ds245518.mlab.com:45518/microthread-prod"}
+    module.exports = {mongoURI: "mongodb://micro_am_user:D9w_VybC6@ds245518.mlab.com:45518/microthread-prod"}
 } else {
     module.exports = {mongoURI: "mongodb://localhost/microthread-dev"}
 }
