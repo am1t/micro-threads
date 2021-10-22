e41cbb5f8d9745d77e36c4a4df65071ba88b1520
Amit Gawande <amit.w.gawande@gmail.com>
2021-10-22 15:30:05 +0530

    Fixed issue with Crypto usage

--- config/appconfig.js
+++ config/appconfig.js
@@ -1,6 +1,6 @@
 if(process.env.NODE_ENV === 'production'){
     module.exports = {
-        /*seckey: process.env.SECKEY,*/
+        seckey: process.env.SECKEY,
         session_secret: process.env.SESSION_SECRET,
         redis_url: process.env.REDIS_URL
     }
